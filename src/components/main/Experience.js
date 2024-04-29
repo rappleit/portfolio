018b93896d23c5a67c719cdf88801e52788f7587
Rachel <80181684+rappleit@users.noreply.github.com>
2024-04-29 09:55:05 +0800

    Added all projects banner

---
+++ src/components/main/Experience.js
@@ -0,0 +1,7 @@
+const Experience = () => {
+    return ( 
+        <div className=""></div>
+     );
+}
+ 
+export default Experience;
--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -4,8 +4,24 @@ import { FaMobileScreen } from "react-icons/fa6";
 import { FaLaptopCode } from "react-icons/fa";
 import { IoExtensionPuzzle } from "react-icons/io5";
 import FeaturedProjects from "./FeaturedProjects";
-
+import { useEffect, useState } from "react";
+import archive from "../../assets/main/archive.png"
+import TextTransition, { presets } from 'react-text-transition';
+import { Link } from "react-router-dom";
 const Projects = () => {
+
+
+    const TEXTS = ['A medical report assistant', 'An upskilling roadmap generator', 'A social todo list app', 'A React Simon Says Game'];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+      const intervalId = setInterval(
+        () => setIndex((index) => index + 1),
+        3000, // every 3 seconds
+      );
+      return () => clearTimeout(intervalId);
+    }, []);
+
     return (
         <div className={styles.main}>
             <div className={styles.header}>
@@ -53,7 +69,16 @@ const Projects = () => {
                 <div className={styles.featuredContainer}>
                     <h2>Featured Projects</h2>
                     <FeaturedProjects />
-
+                </div>
+                <div className={styles.archiveContainer}>
+                    <img src={archive} alt="" />
+                    <div className={styles.archiveContent}>
+                        <h2>View all my projects <Link to="https://github.com/rappleit" target="_blank" rel="noopener noreferrer" className={styles.archiveLink}>here</Link></h2>
+                        <div className={styles.archivePlayer}>
+                        <p>{'>'} </p>
+                        <TextTransition  inline>{TEXTS[index % TEXTS.length]}</TextTransition>
+                        </div>
+                    </div>
                 </div>
 
             </div>
