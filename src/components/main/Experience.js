c4cee0a6350dd05b8e8da68aec18b4210b1cd3d6
Rachel <80181684+rappleit@users.noreply.github.com>
2024-05-08 03:56:37 +0800

    added experience ipad/desktop section

--- src/components/main/Experience.js
+++ src/components/main/Experience.js
@@ -32,7 +32,7 @@ const Experience = () => {
                 "Leading the organizing committee of the university’s annual hackathon, What The Hack 2024",
                 "Designed and developed websites for the university's events such as SUTD's annual hackathon and night festival."
             ]
-        }, 
+        },
         {
             title: "Full-stack Developer",
             team: "SUTD Google Developer Student Club (3DC)",
@@ -42,7 +42,7 @@ const Experience = () => {
                 "Developed web and mobile projects that achieved recognition in several hackathons",
                 "Hosted 2 workshops on technologies such as repository management with Git and Github, and Object Oriented Programming with Python"
             ]
-        }, 
+        },
         {
             title: "Developer",
             team: "SUTD campusX Builders",
@@ -51,7 +51,7 @@ const Experience = () => {
             content: [
                 "Currently designing and developing a ReactJS-based forum application within the Microsoft Teams environment, utilizing OpenAI and Question Answering AI technologies to provide automated responses to student queries on computational thinking modules"
             ]
-        }, 
+        },
         {
             title: "Beta Ambassador",
             team: "Microsoft Student Learn Ambassadors",
@@ -62,7 +62,7 @@ const Experience = () => {
                 "Conducted an online workshop on web accessibility"
 
             ]
-        }, 
+        },
     ]
 
     const professionalExperienceList = [
@@ -74,7 +74,7 @@ const Experience = () => {
             content: [
                 "Instructed students across more than 17 primary and secondary schools in various computational skills, including app development in Swift, IOT with M5Go, drone programming, game development, and algorithms"
             ]
-        }, 
+        },
         {
             title: "Education Technology Intern Developer",
             team: "Tinkertanker Pte. Ltd.",
@@ -84,7 +84,7 @@ const Experience = () => {
                 "Redesigned and built a Next.js learning management system web application that serves as a user-friendly dashboard for instructors to assign and monitor student work",
                 "Collaborated with Apple to design curriculum, develop web marketing material, and produce instructional videos for the Swift Explorers’ Singapore, a pioneering app design programme for students"
             ]
-        }, 
+        },
     ]
     return (
         <div className={styles.main}>
@@ -99,20 +99,84 @@ const Experience = () => {
             <div className={`${styles.content_mobile} ${(selectedCategory === 'professional') ? styles.content_professional : styles.content_extracurricular}`}>
                 {(selectedCategory === "professional") ? professionalExperienceList.map((item, i) => (
                     <ExperienceCardMobile {...item}
-                    type={selectedCategory}
-                    expanded={expandedIndex === i}
-                    onToggle={() => handleCardToggle(i)}
-                    key={i}/>
+                        type={selectedCategory}
+                        expanded={expandedIndex === i}
+                        onToggle={() => handleCardToggle(i)}
+                        key={i} />
                 )) : extracurricularExperienceList.map((item, i) => (
                     <ExperienceCardMobile {...item}
-                    type={selectedCategory}
-                    expanded={expandedIndex === i}
-                    onToggle={() => handleCardToggle(i)}
-                    key={i}/>
+                        type={selectedCategory}
+                        expanded={expandedIndex === i}
+                        onToggle={() => handleCardToggle(i)}
+                        key={i} />
                 ))}
             </div>
             <div className={styles.content}>
-                
+                <div className={`${styles.selectorMenu}`}>
+                    <div className={styles.selectorButtons}>
+                        <button onClick={() => handleCategoryChange()} className={(selectedCategory === "professional") ? styles.professionalSelected : ""}>Professional</button>
+                        <button onClick={() => handleCategoryChange()} className={(selectedCategory === "extracurricular") ? styles.extracurricularSelected : ""}>Extracurricular</button>
+                    </div>
+                    <div className={styles.selectorList}>
+                        {(selectedCategory === "professional") ? professionalExperienceList.map((item, i) => (
+                            <div
+                                className={`${styles.experienceCard} ${styles.experienceCardProfessional}  ${(expandedIndex === i) ? styles.selectedCard : ""}`}
+                                key={i}
+                                onClick={() => handleCardToggle(i)}>
+                                <p className={styles.experienceCardDate}>{item.duration}</p>
+                                <h3>{item.title}</h3>
+                                <p className={styles.experienceCardTeam}>{item.team}</p>
+                            </div>
+                        )) : extracurricularExperienceList.map((item, i) => (
+                            <div
+                                className={`${styles.experienceCard} ${styles.experienceCardExtracurricular} ${(expandedIndex === i) ? styles.selectedCard : ""}`}
+                                key={i}
+                                onClick={() => handleCardToggle(i)}>
+                                <p className={styles.experienceCardDate}>{item.duration}</p>
+                                <h3>{item.title}</h3>
+                                <p className={styles.experienceCardTeam}>{item.team}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={`${styles.contentContainer}`}>
+                    <div className={styles.contentHeader}>
+                        <h3>{'>'} Experience</h3>
+                    </div>
+                    <div className={styles.contentBody}>
+                        {(expandedIndex != null) ?
+                            <>
+                                {(selectedCategory === "professional") ?
+                                    <>
+                                        <h2>{professionalExperienceList[expandedIndex].title}</h2>
+                                        <p className={styles.contentBodyTeam}>{professionalExperienceList[expandedIndex].team}</p>
+                                        <p className={styles.contentBodyDuration}>{professionalExperienceList[expandedIndex].duration}</p>
+                                        <p className={styles.contentBodySubtitle}>{(professionalExperienceList[expandedIndex].subtitle != null) ? professionalExperienceList[expandedIndex].subtitle : ""}</p>
+                                        <ul className={styles.contentBodyDetails}>
+                                            {professionalExperienceList[expandedIndex].content.map((item, i) => (
+                                                <li key={i}>{item}</li>
+                                            ))}
+                                        </ul>
+                                    </>
+                                    :
+                                    <>
+                                        <h2>{extracurricularExperienceList[expandedIndex].title}</h2>
+                                        <p className={styles.contentBodyTeam}>{extracurricularExperienceList[expandedIndex].team}</p>
+                                        <p className={styles.contentBodyDuration}>{extracurricularExperienceList[expandedIndex].duration}</p>
+                                        <p className={styles.contentBodySubtitle}>{(extracurricularExperienceList[expandedIndex].subtitle != null) ? extracurricularExperienceList[expandedIndex].subtitle : ""}</p>
+                                        <ul className={styles.contentBodyDetails}>
+                                            {extracurricularExperienceList[expandedIndex].content.map((item, i) => (
+                                                <li key={i}>{item}</li>
+                                            ))}
+                                        </ul>
+                                    </>
+                                }
+                            </>
+                            : 
+                            <p>Please select an item</p>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
